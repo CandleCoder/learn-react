bf062fd12546b1a2cd482d03449466ff3a8681cb
Manikant Upadhyay <manikantupadhyay@gmail.com>
2021-05-15 19:42:48 +0530

    Adding basic Custome Components

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,4 +1,13 @@
+import './ExpenseItem.css'
 function ExpenseItem() {
-    return <h2> Expense Items!!</h2>;
+    return (
+        <div className = "expense-item">
+            <div>MAY 15 2021</div>
+            <div className = "expense-item__description">
+                <h2>Drinks</h2>
+                <div className = "expense-item__price">$ 700.98</div>
+            </div>
+        </div>
+    );
 }
 export default ExpenseItem;
