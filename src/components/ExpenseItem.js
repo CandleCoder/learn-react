7a04ed1bc47e9a7fb204c46b601a4f048aa9f365
Manikant Upadhyay <manikantupadhyay@gmail.com>
2021-05-16 10:24:32 +0530

    Adding Dynamic data

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,14 +1,18 @@
 import './ExpenseItem.css'
-function ExpenseItem() {
-    const expenseDate = new Date(2021, 3 , 25);
-    const expenseName = 'Drinks';
-    const expenseCost = 4568;
+function ExpenseItem(props) {
+    const month = props.date.toLocaleString('en-US', {month: 'long'});
+    const day = props.date.toLocaleString('en-US', {day: '2-digit'});
+    const year = props.date.getFullYear();
     return (
         <div className = "expense-item">
-            <div>{expenseDate.toISOString()}</div>
+            <div>
+                <div>{month}</div>
+                <div>{day}</div>
+                <div>{year}</div>
+            </div>
             <div className = "expense-item__description">
-                <h2>{expenseName}</h2>
-                <div className = "expense-item__price">${expenseCost}</div>
+                <h2>{props.title}</h2>
+                <div className = "expense-item__price">${props.amount}</div>
             </div>
         </div>
     );
