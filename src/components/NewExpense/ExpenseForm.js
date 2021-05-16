94febb9f4d8701cd34974070045e27b5b2c4e759
Manikant Upadhyay <manikantupadhyay@gmail.com>
2021-05-16 19:28:45 +0530

    Form Added

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,20 +1,50 @@
+import {useState} from 'react'
+
 import './ExpenseForm.css';
+
 const ExpenseForm = () => {
+    const [userInput, setUserInput] = useState({
+        setEnteredTitle: '',
+        setEnteredAmount: '',
+        setEnteredDate: ''
+    })
+
+    const titleChangeHandler = (event) => {
+        setUserInput({
+            ...userInput,
+            setEnteredTitle: event.target.value
+        });
+    }
+
+    const amounthangeHandler = (event) => {
+        setUserInput({
+            ...userInput,
+            setEnteredAmount: event.target.value
+        });
+    }
+
+    const datehangeHandler = (event) => {
+        setUserInput({
+            ...userInput,
+            setEnteredDate: event.target.value
+        });
+    }
+
     return (
         <div className ='new-Expense'>
         <form>
             <div className = 'new-expense__controls'>
                 <div className = 'new-expense__control'>
                     <label>Title</label>
-                    <input type = 'text'></input>
+                    <input type = 'text' onChange = {titleChangeHandler}></input>
                 </div>
                 <div className = 'new-expense__control'>
                     <label>Amount</label>
-                    <input type = 'number' min = '0.01' step = '0.01'></input>
+                    <input type = 'number' min = '0.01' step = '0.01' onChange = {amounthangeHandler}></input>
                 </div>
                 <div className = 'new-expense__control'>
                     <label>Date</label>
-                    <input type = 'date' min = '2019-01-01' max = '2022-12-31'></input>
+                    <input type = 'date' min = '2019-01-01' max = '2022-12-31' onChange = {datehangeHandler}></input>
                 </div>
             </div>
             <div className = 'new-expense__actions'>
