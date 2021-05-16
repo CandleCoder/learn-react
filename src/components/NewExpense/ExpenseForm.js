687b4435837e9c910b0f6bbb8aadc1fe5ee87248
Manikant Upadhyay <manikantupadhyay@gmail.com>
2021-05-16 20:54:10 +0530

    Singleton Multi State Handling

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -2,7 +2,7 @@ import {useState} from 'react'
 
 import './ExpenseForm.css';
 
-const ExpenseForm = () => {
+const ExpenseForm = (props) => {
     // Multi State Combined Approach
     const [enteredTitle, setEnteredTitle] = useState('');
     const [enteredAmount, setEnteredAmount] = useState('');
@@ -12,29 +12,42 @@ const ExpenseForm = () => {
         setEnteredTitle(event.target.value);
     }
 
-    const amounthangeHandler = (event) => {
+    const amountchangeHandler = (event) => {
         setEnteredAmount(event.target.value);
     }
 
-    const datehangeHandler = (event) => {
+    const datechangeHandler = (event) => {
         setEnteredDate(event.target.value);
+    }
+
+    const formSumbitHandler = (event) => {
+        event.preventDefault();
+        const expenseData = {
+            title: enteredTitle,
+            amout: enteredAmount,
+            Date: new Date(enteredDate)
+        }
+        console.log(expenseData)
+        setEnteredAmount('');
+        setEnteredTitle('');
+        setEnteredDate('');
     }
 
     return (
         <div className ='new-Expense'>
-        <form>
+        <form onChange = {formSumbitHandler}>
             <div className = 'new-expense__controls'>
                 <div className = 'new-expense__control'>
                     <label>Title</label>
-                    <input type = 'text' onChange = {titleChangeHandler}></input>
+                    <input type = 'text' value = {enteredTitle} onChange = {titleChangeHandler}></input>
                 </div>
                 <div className = 'new-expense__control'>
                     <label>Amount</label>
-                    <input type = 'number' min = '0.01' step = '0.01' onChange = {amounthangeHandler}></input>
+                    <input type = 'number' min = '0.01' step = '0.01' value = {enteredAmount} onChange = {amountchangeHandler}></input>
                 </div>
                 <div className = 'new-expense__control'>
                     <label>Date</label>
-                    <input type = 'date' min = '2019-01-01' max = '2022-12-31' onChange = {datehangeHandler}></input>
+                    <input type = 'date' min = '2019-01-01' max = '2022-12-31' value = {enteredDate} onChange = {datechangeHandler}></input>
                 </div>
             </div>
             <div className = 'new-expense__actions'>
